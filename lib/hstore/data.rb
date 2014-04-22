440fdc3fe88cc9c5281cb435f029db8780eb693e
Marcelo Eden <edendroid@gmail.com>
2014-04-21 19:06:14 -0700

    init gem

---
+++ lib/hstore/data.rb
@@ -0,0 +1,7 @@
+require "hstore/data/version"
+
+module Hstore
+  module Data
+    # Your code goes here...
+  end
+end
---
+++ lib/hstore/data/version.rb
@@ -0,0 +1,5 @@
+module Hstore
+  module Data
+    VERSION = "0.0.1"
+  end
+end
